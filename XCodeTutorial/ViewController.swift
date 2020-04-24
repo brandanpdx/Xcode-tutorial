37210438053b4f8f880771a99ace725f5205439a
Brandan Sayarath <brandan@brandan.tech>
2020-04-23 23:13:55 -0700

    Initial Commit

---
+++ XCodeTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeTutorial
+//
+//  Created by Brandan Sayarath on 4/23/20.
+//  Copyright © 2020 Brandan Sayarath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
